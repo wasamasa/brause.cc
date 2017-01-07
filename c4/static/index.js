8ffe501c356430a5d6327b3389b28d6e1c302932
Vasilij Schneidermann <v.schneidermann@gmail.com>
2017-01-07 17:37:16 +0100

    Enhance with JS

---
+++ c4/static/index.js
@@ -0,0 +1,32 @@
+window.onload = function() {
+    var apiUrl = 'https://api.koeln.ccc.de/';
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+        var res = JSON.parse(this.responseText);
+        var last = res.state.lastchange;
+        var now = Date.now() / 1000;
+        var delta = now - last;
+
+        var description = 'Seit etwa ';
+        if (delta > 2*365*24*60*60) {
+            description += Math.round(delta/(365*24*60*60)) + ' Jahren';
+        } else if (delta > 2*30*24*60*60) {
+            description += Math.round(delta/(30*24*60*60)) + ' Monaten';
+        } else if (delta > 2*24*60*60) {
+            description += Math.round(delta/(24*60*60)) + ' Tagen';
+        } else if (delta > 2*60*60) {
+            description += Math.round(delta/(60*60)) + ' Stunden';
+        } else if (delta > 2*60) {
+            description += Math.round(delta/60) + ' Minuten';
+        } else {
+            description += Math.round(delta) + ' Sekunden';
+        }
+
+        var detail = document.createElement('h2');
+        detail.appendChild(document.createTextNode(description));
+        var body = document.getElementsByTagName('body')[0];
+        body.appendChild(detail);
+    }
+    xhr.open('GET', apiUrl);
+    xhr.send();
+}
