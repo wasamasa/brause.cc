08d16a12ba61d5a40b5d6ab3e085471b44f105d7
Vasilij Schneidermann <v.schneidermann@gmail.com>
2017-01-08 23:08:18 +0100

    Add special cases

--- c4/static/index.js
+++ c4/static/index.js
@@ -10,16 +10,26 @@ window.onload = function() {
         var delta = now - last;
 
         var description = 'Seit etwa ';
-        if (delta > 2*365*24*60*60) {
+        if (delta >= 2*365*24*60*60) {
             description += Math.round(delta/(365*24*60*60)) + ' Jahren';
-        } else if (delta > 2*30*24*60*60) {
+        } else if (delta >= 365*24*60*60) {
+            description += 'einem Jahr';
+        } else if (delta >= 2*30*24*60*60) {
             description += Math.round(delta/(30*24*60*60)) + ' Monaten';
-        } else if (delta > 2*24*60*60) {
+        } else if (delta >= 30*24*60*60) {
+            description += 'einem Monat';
+        } else if (delta >= 2*24*60*60) {
             description += Math.round(delta/(24*60*60)) + ' Tagen';
-        } else if (delta > 2*60*60) {
+        } else if (delta >= 24*60*60) {
+            description += 'einem Tag';
+        } else if (delta >= 2*60*60) {
             description += Math.round(delta/(60*60)) + ' Stunden';
-        } else if (delta > 2*60) {
+        } else if (delta >= 60*60) {
+            description += 'einer Stunde';
+        } else if (delta >= 2*60) {
             description += Math.round(delta/60) + ' Minuten';
+        } else if (delta >= 60) {
+            description += 'einer Minute';
         } else {
             description += Math.round(delta) + ' Sekunden';
         }
