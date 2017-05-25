6e2bd5e4e8fb2ad69c40b8e07e142a09a3561259
Vasilij Schneidermann <mail@vasilij.de>
2017-05-25 21:21:09 +0200

    Disable user scaling (for devices honoring it)

--- c4/open.scm
+++ c4/open.scm
@@ -62,7 +62,9 @@
                 (head
                  (meta (@ (charset "utf-8")))
                  (meta (@ (name "viewport")
-                          (content "initial-scale=1.0,  width=device-width")))
+                          (content ,(string-append "initial-scale=1.0,"
+                                                   "width=device-width,"
+                                                   "user-scalable=no"))))
                  (title "Hat der C4 offen?")
                  (link (@ (href "style.css")
                           (rel "stylesheet")
